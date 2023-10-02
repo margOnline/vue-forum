deead00ee0db201e4427baa3ea4b4a2277cab5ad
Margo Urey <margo@margonline.co.uk>
2023-10-02 12:32:08 +0100

    persisting new threads to firestore

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,21 +1,30 @@
-import * as firestore from 'firebase/firestore'
+import {
+  arrayUnion,
+  collection,
+  doc,
+  getDoc,
+  getDocs,
+  serverTimestamp,
+  writeBatch
+}
+from 'firebase/firestore'
 import db from '@/config/firebase'
 import { findById } from '@/helpers'
 
 export default {
   async createPost ({ commit, state }, post) {
     post.userId = state.authId
-    post.publishedAt = firestore.serverTimestamp()
-    const batch = firestore.writeBatch(db)
-    const postRef = firestore.doc(firestore.collection(db, 'posts'))
-    const threadRef = firestore.doc(db, 'threads', post.threadId)
+    post.publishedAt = serverTimestamp()
+    const batch = writeBatch(db)
+    const postRef = doc(collection(db, 'posts'))
+    const threadRef = doc(db, 'threads', post.threadId)
     batch.set(postRef, post)
     batch.update(threadRef, {
-      posts: firestore.arrayUnion(postRef.id),
-      contributors: firestore.arrayUnion(state.authId)
+      posts: arrayUnion(postRef.id),
+      contributors: arrayUnion(state.authId)
     })
     await batch.commit()
-    const newPost = await firestore.getDoc(postRef)
+    const newPost = await getDoc(postRef)
 
     commit('setItem', { resource: 'posts', item: { ...post, id: newPost.id } })
     commit(
@@ -24,20 +33,28 @@ export default {
     commit(
       'appendContributorToThread', { parentId: post.threadId, childId: state.authId }
     )
-  },
-  updateUser ({ commit }, user) {
-    commit('setItem', { resource: 'users', item: user })
+    return findById(state.posts, postRef.id)
   },
   async createThread ({ commit, state, dispatch }, { title, text, forumId }) {
-    const id = 'ffff' + Math.random()
     const userId = state.authId
-    const publishedAt = Math.floor(Date.now() / 1000)
-    const thread = { forumId, title, text, publishedAt, userId, id }
-    commit('setItem', { resource: 'threads', item: thread })
-    commit('appendThreadToForum', { parentId: forumId, childId: id })
-    commit('appendThreadToUser', { parentId: userId, childId: id })
-    dispatch('createPost', { text, threadId: id })
-    return state.threads.find(thread => thread.id === id)
+    const publishedAt = serverTimestamp()
+    const threadRef = doc(collection(db, 'threads'))
+    const userRef = doc(db, 'users', userId)
+    const forumRef = doc(db, 'forums', forumId)
+    const thread = { forumId, title, text, publishedAt, userId, id: threadRef.id }
+    const batch = writeBatch(db)
+
+    batch.set(threadRef, thread)
+    batch.update(userRef, { threads: arrayUnion(threadRef.id) })
+    batch.update(forumRef, { threads: arrayUnion(threadRef.id) })
+    await batch.commit()
+    const newThread = await getDoc(threadRef)
+
+    commit('setItem', { resource: 'threads', item: { ...newThread.data, id: newThread.id } })
+    commit('appendThreadToForum', { parentId: forumId, childId: threadRef.id })
+    commit('appendThreadToUser', { parentId: userId, childId: threadRef.id })
+    await dispatch('createPost', { text, threadId: threadRef.id })
+    return findById(state.threads, threadRef.id)
   },
   async updateThread ({ commit, state }, { title, text, id }) {
     const thread = findById(state.threads, id)
@@ -49,6 +66,9 @@ export default {
     commit('setItem', { resource: 'posts', item: newPost })
     return newThread
   },
+  updateUser ({ commit }, user) {
+    commit('setItem', { resource: 'users', item: user })
+  },
   fetchCategory: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'categories', id, emoji: '🏷' }),
   fetchForum: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'forums', id, emoji: '🏁' }),
   fetchThread: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'threads', id, emoji: '📄' }),
@@ -56,8 +76,8 @@ export default {
   fetchUser: ({ dispatch }, { id }) => dispatch('fetchItem', { resource: 'users', id, emoji: '🙋' }),
   fetchAuthUser: ({ dispatch, state }) => dispatch('fetchUser', { id: state.authId }),
   async fetchAllCategories ({ commit }) {
-    const collectionRef = firestore.collection(db, 'categories')
-    const collectionSnap = await firestore.getDocs(collectionRef)
+    const collectionRef = collection(db, 'categories')
+    const collectionSnap = await getDocs(collectionRef)
     return collectionSnap.docs.map((doc) => {
       const item = { id: doc.id, ...doc.data() }
       commit('setItem', { resource: 'categories', item })
@@ -70,8 +90,8 @@ export default {
   fetchPosts: ({ dispatch }, { ids }) => dispatch('fetchItems', { ids, resource: 'posts', emoji: '💬' }),
   fetchUsers: ({ dispatch }, { ids }) => dispatch('fetchItems', { ids, resource: 'users', emoji: '🙋' }),
   async fetchItem ({ state, commit }, { id, emoji, resource }) {
-    const itemRef = firestore.doc(db, resource, id)
-    const itemSnap = await firestore.getDoc(itemRef)
+    const itemRef = doc(db, resource, id)
+    const itemSnap = await getDoc(itemRef)
     const item = { ...itemSnap.data(), id: itemRef.id }
     commit('setItem', { resource, id, item })
     return item
