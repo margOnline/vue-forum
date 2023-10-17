d29f471886e369a224ac541fd539265802bb0d62
Margo Urey <margo@margonline.co.uk>
2023-10-17 15:06:47 +0100

    Refactor: update the global components vis a vis the modularised store

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -22,7 +22,7 @@
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
               <li class="dropdown-menu-item"><router-link :to="{ name: 'Profile'}">View profile</router-link></li>
-              <li class="dropdown-menu-item"><a @click.prevent="$store.dispatch('signOut')">Sign Out</a></li>
+              <li class="dropdown-menu-item"><a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a></li>
             </ul>
           </div>
         </li>
@@ -59,7 +59,7 @@ import { mapGetters } from 'vuex'
 
 export default {
   computed: {
-    ...mapGetters(['authUser'])
+    ...mapGetters('auth', ['authUser'])
   },
   data () {
     return {
