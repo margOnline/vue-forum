c909d9c923a0119f5bda14a7916988a0522bdc3f
Margo Urey <margo@margonline.co.uk>
2023-10-19 14:20:34 +0100

    User experience: improve the navigation bar
    - Add a mobile hamburger menu, ensure it opens on click and closes
    when clicking outside the menu by adding our custom directive v-click-outside.
    - On sign out, redirect the user to the home page and ensure the menu
    is closed.

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,11 +1,18 @@
 <template>
-  <header class="header" id="header">
-
+  <header class="header" id="header" v-click-outside="()=> mobileNavMenu=false">
     <router-link class="logo" :to="{ name: 'Home'}">
       <img src="../assets/svg/vueschool-logo.svg">
     </router-link>
 
-    <nav class="navbar">
+    <div class="btn-hamburger" @click="mobileNavMenu = !mobileNavMenu">
+      <!-- use .btn-humburger-active to open the menu -->
+      <div class="top bar"></div>
+      <div class="middle bar"></div>
+      <div class="bottom bar"></div>
+    </div>
+
+    <!-- use .navbar-open to open nav -->
+    <nav class="navbar" :class="{ 'navbar-open': mobileNavMenu}">
       <ul>
         <li v-if="authUser" class="navbar-user">
           <a
@@ -25,12 +32,22 @@
             <div class="triangle-drop"></div>
             <ul class="dropdown-menu">
               <li class="dropdown-menu-item"><router-link :to="{ name: 'Profile'}">View profile</router-link></li>
-              <li class="dropdown-menu-item"><a @click.prevent="$store.dispatch('auth/signOut')">Sign Out</a></li>
+              <li class="dropdown-menu-item">
+                <a @click.prevent="$store.dispatch('auth/signOut'), mobileNavMenu=false">
+                  Sign Out
+                </a>
+              </li>
             </ul>
           </div>
         </li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{ name: 'SignIn'}">Sign in</router-link></li>
         <li v-if="!authUser" class="navbar-item"><router-link :to="{ name: 'Register'}">Register</router-link></li>
+        <li v-if="authUser" class="navbar-mobile-item"><router-link :to="{name: 'Profile'}">View Profile</router-link></li>
+        <li v-if="authUser" class="navbar-mobile-item">
+          <a @click.prevent="$store.dispatch('auth/signOut'), mobileNavMenu=false">
+            Sign Out
+          </a>
+        </li>
       </ul>
       <!--<ul>
         <li class="navbar-item">
@@ -66,8 +83,14 @@ export default {
   },
   data () {
     return {
-      userDropdownOpen: false
+      userDropdownOpen: false,
+      mobileNavMenu: false
     }
+  },
+  created () {
+    this.$router.beforeEach((to, from) => {
+      this.mobileNavMenu = false
+    })
   }
 }
 </script>
