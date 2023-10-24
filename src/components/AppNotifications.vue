41f63c4ff320d365a64f5b90588b64e944b647eb
Margo Urey <margo@margonline.co.uk>
2023-10-24 16:13:31 +0100

    User experience: make notifications glide in and out for a nicer experience
    - Use transitions to achieve a nicer visual
    - Move notification styles from the global style sheet to the notification component

--- src/components/AppNotifications.vue
+++ src/components/AppNotifications.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="notifications">
-    <div class="notification" v-for="notification in notifications" :key="notification.id">
-      <span>{{ notification.message }}</span>
-      <button @click="removeNotification(notification.id)">x</button>
-    </div>
+    <transition-group name="notification">
+      <div class="notification" v-for="notification in notifications" :key="notification.id">
+        <span>{{ notification.message }}</span>
+        <button @click="removeNotification(notification.id)">x</button>
+      </div>
+    </transition-group>
   </div>
 </template>
 
@@ -17,3 +19,31 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.notifications {
+    position: fixed;
+    bottom: 20px;
+    right: 0;
+}
+.notification {
+    background: white;
+    display: flex;
+    justify-content: space-between;
+    width: 350px;
+    box-shadow: 2px 2px 2px 2px rbga(0, 0, 0.5);
+    padding: 10px 20px;
+    margin-bottom: 5px;
+    border-left: 5px solid #263959;
+}
+.notification-enter-active, .notification-enter-leave {
+  transition: all .5s ease;
+}
+.notification-enter-from, .notification-leave-to {
+  opacity: 0;
+  transform: transalateX(100%)
+}
+.notification-move {
+  transition: transform 0.8s ease;
+}
+</style>
